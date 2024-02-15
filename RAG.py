cf7aec71a1bc7ceb4753e37faa6d89160af4b94a
forusecase1 <157874501+forusecase1@users.noreply.github.com>
2024-02-15 09:38:06 +0800

    Update RAG.py

--- RAG.py
+++ RAG.py
@@ -19,7 +19,24 @@ app = Flask(__name__)
 
 load_dotenv()
 
+# domain root
+@app.route('/')
+def home():
+    return 'Hello, World!'
+
 @app.route("/webhook", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    # handle webhook body
+    try:
+        line_handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
 
 model_type= 'gemini'
 
@@ -108,3 +125,21 @@ if prompt:
 
         with st.chat_message("Assistant"):
             st.markdown(response['result'])
+
+@line_handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    global working_status
+    if event.message.type != "text":
+        return
+
+    if working_status:
+        chatgpt.add_msg(f"HUMAN:{event.message.text}?\n")
+        reply_msg =  st.markdown(response['result'])
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=reply_msg))
+
+if __name__ == "__main__":
+    app.run()
+
+
